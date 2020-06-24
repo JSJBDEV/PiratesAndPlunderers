0c9cab7c172389c1a1ed5b6c4fae88978917c0ff
Acrogenous <acrodynasty@gmail.com>
2020-06-24 15:53:04 +0100

    added grappler item to start engagment

--- src/main/java/gd/rf/acro/blockwake/ClientInit.java
+++ src/main/java/gd/rf/acro/blockwake/ClientInit.java
@@ -3,13 +3,15 @@ package gd.rf.acro.blockwake;
 import gd.rf.acro.blockwake.entities.PirateEntityRenderer;
 import gd.rf.acro.blockwake.entities.SailingShipMobEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+
+import javax.swing.text.JTextComponent;
 
 public class ClientInit implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         EntityRendererRegistry.INSTANCE.register(Blockwake.SAILING_BOAT_ENTITY_ENTITY_TYPE, (entityRenderDispatcher, context) -> new SailingShipMobEntityRenderer(entityRenderDispatcher));
         EntityRendererRegistry.INSTANCE.register(Blockwake.PIRATE_ENTITY_ENTITY_TYPE, (entityRenderDispatcher, context) -> new PirateEntityRenderer(entityRenderDispatcher));
-
     }
 }
